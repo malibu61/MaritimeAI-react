8e1db02db4485c3df00e7e9c6e1f2584b309fa18
Muhammet Ali TANRIKULU <muhammedd45@icloud.com>
2025-10-01 21:47:12 +0300

    test: datas come from api tested

--- src/pages/ships.js
+++ src/pages/ships.js
@@ -1,21 +1,46 @@
 import {getAllShipsService, getShipsWZoom1Service} from "../services/ships";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const Ships = () => {
 
     const [ships, setShips] = useState(null)
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         getShipsWZoom1()
     }, [])
 
     const getShipsWZoom1 = () => {
-        getShipsWZoom1Service().then(response => {console.log(response)})
+        getShipsWZoom1Service().then(response => {
+            setShips(response);
+            setLoading(false)
+            console.log(response)
+        })
     }
 
     return (
 
         <>
             <h1>ShipsPage</h1>
+
+            {loading ? (
+                <div>
+                    ...Yükleniyor...
+                </div>
+            ) : ships.map((item, index) => {
+                return (
+                    <React.Fragment key={index}>
+
+                        <p>MMSI: {item.MMSI}</p>
+                        <p>Ad: {item.Name}</p>
+                        <p>Latitude: {item.Latitude}</p>
+                        <p>Longitude: {item.Longitude}</p>
+
+                    </React.Fragment>
+
+                )
+            })}
+
+
         </>
     )
 
